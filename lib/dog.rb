bd13cfeda59c917c4953fab7fa25ee15d0b2ab31
Megan Horsfield <meganthadev@gmail.com>
2020-03-23 05:16:57 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -47,6 +47,11 @@ class Dog
     end.first
   end
   
-  
+  def update 
+    sql = "UPDATE dogs SET name = ?, breed = ?, WHERE id = ?" 
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+   end 
+  end 
     
-  end   
+    
+end   
